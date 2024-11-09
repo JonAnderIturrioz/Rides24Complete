41e9830265dc3f18561b0a981c67371c3463a123
JonAnderIturrioz <jiturrioz08@gmail.com>
2024-11-09 22:54:54 +0100

    Business Logic Factory Method pattern applied

--- src/main/java/gui/ApplicationLauncher.java
+++ src/main/java/gui/ApplicationLauncher.java
@@ -1,16 +1,12 @@
 package gui;
 
-import java.net.URL;
 import java.util.Locale;
 
 import javax.swing.UIManager;
-import javax.xml.namespace.QName;
-import javax.xml.ws.Service;
 
-import configuration.ConfigXML;
-import dataAccess.DataAccess;
 import businessLogic.BLFacade;
-import businessLogic.BLFacadeImplementation;
+import businessLogic.BusinessLogicFactory;
+import configuration.ConfigXML;
 
 public class ApplicationLauncher {
 
@@ -29,28 +25,9 @@ public class ApplicationLauncher {
 			BLFacade appFacadeInterface;
 			UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
 
-			if (c.isBusinessLogicLocal()) {
-
-				DataAccess da = new DataAccess();
-				appFacadeInterface = new BLFacadeImplementation(da);
-
-			}
-
-			else { // If remote
-
-				String serviceName = "http://" + c.getBusinessLogicNode() + ":" + c.getBusinessLogicPort() + "/ws/"
-						+ c.getBusinessLogicName() + "?wsdl";
-
-				URL url = new URL(serviceName);
-
-				// 1st argument refers to wsdl document above
-				// 2nd argument is service name, refer to wsdl document above
-				QName qname = new QName("http://businessLogic/", "BLFacadeImplementationService");
-
-				Service service = Service.create(url, qname);
-
-				appFacadeInterface = service.getPort(BLFacade.class);
-			}
+			BusinessLogicFactory factory = new BusinessLogicFactory();
+			
+			appFacadeInterface = factory.createBusinessLogic(c);
 
 			MainGUI.setBussinessLogic(appFacadeInterface);
 			MainGUI a = new MainGUI();
